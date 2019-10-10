f20443ee58c57d048872c420ba3233b021dc25f2
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2019-10-10 14:56:39 +0800

    finish boss die end game

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace HeroAndDragons
 {
@@ -7,7 +8,32 @@ namespace HeroAndDragons
         public void Run()
         {
             Console.WriteLine("Hello Game!");
+            var boss = new Boss(100);
+            while (boss.Hp >0)
+            {
+                Thread.Sleep(100);
+                boss.Hp -= 1;
+                Console.WriteLine($"Boss Hp:{boss.Hp}");
+            }
+
+            Console.WriteLine("You Win!");
             Console.ReadLine();
+        }
+    }
+
+    internal class Boss
+    {
+        private int _hp;
+
+        public Boss(int hp)
+        {
+            _hp = hp;
+        }
+
+        public int Hp
+        {
+            get => _hp;
+            set => _hp = value;
         }
     }
 }
